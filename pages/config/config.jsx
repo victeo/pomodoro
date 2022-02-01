7a0782bc0e66181ba1d8d26cc9c8ad46895fa3d3
victeo <victor.atomo@gmail.com>
2022-02-01 23:52:03 +0000

    tela

--- pages/config/config.jsx
+++ pages/config/config.jsx
@@ -13,9 +13,34 @@ export default function Config() {
         <div className={style.container}>
           <h1 className={style.cabecalho}>POMODORO</h1>
         </div>
-        <div className={style.icons}>
-          <BsFillArrowUpCircleFill  size="2rem"  color="#9FA1AE"/>
-          <BsFillArrowDownCircleFill size="2rem"  color="#9FA1AE"/>
+        <div className={style.row}>
+          <div className={style.entrada}>
+            <div className={style.icons}>
+              <BsFillArrowUpCircleFill size="2rem" color="#9FA1AE" />
+              <BsFillArrowDownCircleFill size="2rem" color="#9FA1AE" />
+            </div>
+            <div className={style.input}>
+              <input type="text" name="" id="" />
+            </div>
+          </div>
+          <div className={style.entrada}>
+            <div className={style.icons}>
+              <BsFillArrowUpCircleFill size="2rem" color="#9FA1AE" />
+              <BsFillArrowDownCircleFill size="2rem" color="#9FA1AE" />
+            </div>
+            <div className={style.input}>
+              <input type="text" name="" id="" />
+            </div>
+          </div>
+          <div className={style.entrada}>
+            <div className={style.icons}>
+              <BsFillArrowUpCircleFill size="2rem" color="#9FA1AE" />
+              <BsFillArrowDownCircleFill size="2rem" color="#9FA1AE" />
+            </div>
+            <div className={style.input}>
+              <input type="text" name="" id="" />
+            </div>
+          </div>
         </div>
       </Container>
     </>
